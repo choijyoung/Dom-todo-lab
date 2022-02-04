1a47b56da62bc77c6aeca4ccd8623c2d07c4cba7
Jaeyoung Choi <choi.jae.y@gmail.com>
2022-02-03 23:06:39 -0500

    complete lab

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-console.log('hello!');
 const ipt = document.querySelector('#textBox');
 ipt.addEventListener('click', function(event1) {
 	console.log(event1);
 });
 
 const btn = document.querySelector('#submit-button');
-btn.addEventListener('click', function(event2) {
+btn.addEventListener('click', function() {
 	const addLi = document.createElement('li');
 	const inp = document.querySelector('input');
 	addLi.textContent = inp.value;
@@ -20,8 +19,12 @@ rst.addEventListener('click', function(clear) {
 	document.querySelector('ul').innerHTML = '';
 });
 
-const list = document.querySelector('#todo-list');
-document.querySelector('ul').addEventListener('click', clickDelete)
-function clickDelete(event) {
-  event.target.remove('')
-};
+const list = document.querySelector('#todo-list'); 
+	list.addEventListener('click', function(event){
+		const li = event.target.parentNode;
+    li.removeChild(event.target);
+});
+
+
+
+
